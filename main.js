b3a78978cc717de42eeff64e571056c8dbe40ac5
Mitesh-soni <Miteshsoni5666@gmail.com>
2025-10-29 22:29:33 +0530

    server setup

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const app = express()
 const port = 3000
-
-
+async function condb() {
+    try {
+        mongoose.connect("mongodb://localhost:27017/Employee")
+        console.log("connected sucessfully")
+    } catch (err) {
+        console.log(err)
+    }
+}
+condb();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
