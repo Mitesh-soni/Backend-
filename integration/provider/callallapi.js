f93cdb5b30c6979863922a1d46f125c6697714f1
Mitesh-soni <Miteshsoni5666@gmail.com>
2025-11-08 15:47:48 +0530

    .

--- integration/provider/callallapi.js
+++ integration/provider/callallapi.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import intcmdqt from "../models/intcmdqt.js";
 import { intcmdqtControler } from "../controller/intcmdqt.js";
-export const callAllapiController = async (createuserapiData, integrationData) => {
+export const callAllapiController = async (createuserapiData) => {
   try {
     const { Data, ApiUrl, Method, Token } = await createuserapiData;
     const response = await axios({
@@ -14,17 +14,15 @@ export const callAllapiController = async (createuserapiData, integrationData) =
     })
     
     const newRecord = new intcmdqt({
-      trno: trno,                         // same transaction number
-      status: status + 2,                 // ✅ success → +3
-      jsonpara: Data,                     // request data
-      responseData: response?.data || {}, // API response data
+      trno: trno,                         
+      status: status + 2,                 
+      jsonpara: Data,                     
+      responseData: response?.data || {}, 
       createdAt: new Date(),
     });
 
     // 3️⃣ Save record
     const savedData = await newRecord.save();
-
-    console.log("✅ Success stored in intcmdqt:", savedData);
 
 
     return {
@@ -35,12 +33,11 @@ export const callAllapiController = async (createuserapiData, integrationData) =
   catch (err) {
 const { trno, status } = integrationData;
 
-    // 4️⃣ Prepare record for failed response
     const failedRecord = new intcmdqt({
-      trno: trno,                          // same transaction number
-      status: status + 4,                  // ❌ fail → +4
-      jsonpara: createuserapiData.Data,    // request data
-      responseData: { error: err.message },// store error message
+      trno: trno,                          
+      status: status + 4,                 
+      jsonpara: createuserapiData.Data,    
+      responseData: { error: err.message },
       createdAt: new Date(),
     });
 
